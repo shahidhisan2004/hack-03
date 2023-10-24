77116606fe68dccf2621c577e2932ea9d6e027be
shahidhisan2004 <shahidhisan2004@gmail.com>
2023-10-24 09:59:02 -0700

    print declare

--- dataplan.c
+++ dataplan.c
@@ -13,3 +13,11 @@ int main(int argc, char *argv[]) {
 
     int days_remaining = 30 - current_day + 1;
     double average_daily_use = GB_used_so_far / current_day;
+    int avg = 
+
+    printf("%d days used, %d days remaining\n", current_day - 1, days_remaining);
+    printf("Average daily use: %.3lf GB/day\n", average_daily_use);
+  if (GB_used_so_far > GB_in_plan) {
+        printf("You have already met your limit for this month. Looks like you're getting some overage charges...\n");
+    } else if (average_daily_use > GB_in_plan / 30) 
+    
