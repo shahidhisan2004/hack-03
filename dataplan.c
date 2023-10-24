df08b549f578f8d365df7829a7ea1201b2612e46
shahidhisan2004 <shahidhisan2004@gmail.com>
2023-10-24 09:57:27 -0700

    if condition

--- dataplan.c
+++ dataplan.c
@@ -6,4 +6,10 @@ int main(int argc, char *argv[]) {
      double GB_in_plan = atof(argv[1]);
     int current_day = atoi(argv[2]);
     double GB_used_so_far = atof(argv[3]);
-    
+    if (GB_in_plan <= 0 || current_day < 1 || current_day > 30 || GB_used_so_far < 0) {
+        printf("ERROR: Invalid input values.\n");
+        return 1;
+    }
+
+    int days_remaining = 30 - current_day + 1;
+    double average_daily_use = GB_used_so_far / current_day;
