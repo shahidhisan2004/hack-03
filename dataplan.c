6f51864df62c2eac2021909a3133cc61bf20a024
shahidhisan2004 <shahidhisan2004@gmail.com>
2023-10-24 09:56:39 -0700

    ist commit

---
+++ dataplan.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    
+     double GB_in_plan = atof(argv[1]);
+    int current_day = atoi(argv[2]);
+    double GB_used_so_far = atof(argv[3]);
+    
