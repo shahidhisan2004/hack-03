c8dc6de4d43467537a94b6570889ab679ba8ae98
shahidhisan2004 <shahidhisan2004@gmail.com>
2023-10-24 10:02:19 -0700

    last commit

--- dataplan.c
+++ dataplan.c
@@ -26,4 +26,11 @@ int main(int argc, char *argv[]) {
         printf("Continuing this high usage, you'll exceed your data plan by %.0lf GB.\n", (average_daily_use - GB_in_plan / 30) * days_remaining);
         printf("To stay below your data plan, use no more than %.3lf GB/day.\n", remaining_daily_allowance);
     }
-    
+     else {
+        double remaining_daily_allowance = (GB_in_plan - GB_used_so_far) / days_remaining;
+        printf("You are at or below your average daily use (%.3lf GB/day).\n", GB_in_plan / 30);
+        printf("You can use up to %.3lf GB/day and stay below your data plan limit.\n", remaining_daily_allowance);
+    }
+
+  return 0;
+}
